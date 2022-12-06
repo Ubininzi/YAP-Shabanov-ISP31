bb9229705daf223c36ef3e9b524938348367e0e0
Ubininzi <84396800+Ubininzi@users.noreply.github.com>
2022-12-07 02:28:35 +0400

    Update Metods3.cs

--- Metods3.cs
+++ Metods3.cs
@@ -1,18 +1,21 @@
-static void PrintSum(int[] n) {
-	foreach (int i in n){
-		Console.Write($"{i} ");
-	}
-	Console.WriteLine();
+static void PrintSum(int[] n){
+    int Sum = 0;
+    foreach (int i in n){
+        Sum+=i;
+    }
+    Console.WriteLine(Sum);
 }
-static int[] GetCubes(int start,int end){
-	int[] n=new int[end-start+1];
-	int c = 0;
-	for (int i = start; i <= end; i++,c++){
-		n[c] = i * i;
-	}
-	return n;
+static int[] GetCubes(int start, int end)
+{
+    int[] n = new int[end - start + 1];
+    int c = 0;
+    for (int i = start; i <= end; i++, c++)
+    {
+        n[c] = i * i;
+    }
+    return n;
 }
 
 int startValue = int.Parse(Console.ReadLine());
 int endValue = int.Parse(Console.ReadLine());
-PrintSum(GetCubes(startValue, endValue));  
+PrintSum(GetCubes(startValue, endValue));
